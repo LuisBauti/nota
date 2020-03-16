771c9e15392f9d4de8a987d18d8f6643c22f5150
LuisBauti <luisbautista551@gmail.com>
2020-03-15 22:30:59 -0600

    Commit final

--- notebook.py
+++ notebook.py
@@ -26,3 +26,39 @@ class Notebook:
                 return note
 
         return None
+
+    def modify_note(self, note_id, memo):
+        """
+        Encuentra la nota con el valor del id y modifica
+        el contenido de la misma.
+        """
+        note = self._search_note(note_id)
+
+        if note:
+            note.memo = memo
+            return True
+        else:
+            print("No existe una nota con el id: {0}"
+                  .format(note_id))
+            return False
+
+    def modify_tags_note(self, note_id, tags):
+        """
+        Encuentra la nota con el valor del id y modifica
+        las etiquetas de la misma.
+        """
+        note = self._search_note(note_id)
+
+        if note:
+            note.tags = tags
+            return True
+        else:
+            print("No existe una nota con el id: {0}"
+                  .format(note_id))
+            return False
+
+    def search(self, filter):
+        """
+        Busca todas las notas que satisfacen el filtro enviado.
+        """
+        return [note for note in self.notes if note.search(filter)]
