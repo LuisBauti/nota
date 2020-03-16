e6d6d56df0243c5679d3a7bbf8bfc8fa7d40e8b4
LuisBauti <luisbautista551@gmail.com>
2020-03-15 22:27:20 -0600

    Creacion de la funcion para crea una nueva nota y la funcion para buscar

--- notebook.py
+++ notebook.py
@@ -10,3 +10,19 @@ class Notebook:
     def __init__(self):
         """ Inicializa una libreta vacía """
         self.notes = list()
+
+    def new_note(self, memo, tags=""):
+        """ Crea una nueva nota y la agrega a la libreta """
+        self.notes.append(Note(memo, tags))
+
+    def _search_note(self, note_id):
+        """
+        Busca una nota con el id enviado.
+        Esta función es privada (empieza con _).
+        https://docs.python.org/2/tutorial/classes.html
+        """
+        for note in self.notes:
+            if str(note.id) == str(note_id):
+                return note
+
+        return None
