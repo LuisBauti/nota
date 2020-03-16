4c4c66b3336ad006b7867abec4e044c26b00218d
LuisBauti <luisbautista551@gmail.com>
2020-03-15 22:12:16 -0600

    Creacion de las funciones para poder agregar, modificar y buscar las notas y la funcion para salir

--- blockNotas.py
+++ blockNotas.py
@@ -31,3 +31,36 @@ class Menu:
                 4. Modificar una nota
                 5. Salir
                 """)
+
+    def show_notes(self, notes=None):
+        """ Despliega una nota """
+        if not notes:
+            notes = self.notebook.notes
+
+        for note in notes:
+            print("Id: {0}\nEtiquetas: '{1}'\nContenido: {2}\nFecha: {3}"
+                  .format(note.id, note.tags, note.memo, note.creation_date))
+
+    def search_notes(self):
+        """ Busca una nota mediante un filtro """
+        filter = input("Ingresa el texto de búsqueda: ")
+        note = self.notebook.search(filter)
+        self.show_notes(note)
+
+    def add_note(self):
+        memo = input("Ingrese el contenido de la nota: ")
+        tags = input("Etiquetas para la nota separadas por espacio: ")
+        self.notebook.new_note(memo, tags)
+        print("¡Nueva nota agregada!")
+
+    def modify_note(self):
+        pass
+
+    def exit(self):
+        """ Cierra la aplicación """
+        print("Gracias por utilizar su libreta el día de hoy")
+        sys.exit(0)
+
+if __name__ == "__main__":
+    menu = Menu()
+    menu.run()
